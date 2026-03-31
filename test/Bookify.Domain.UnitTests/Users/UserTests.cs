ecfa4999c0d9312c9593cb96a46a62e4c51718d6
Lahir Ahamed <Lahirahamed902@gmail.com>
2026-03-31 06:37:49 +0300

    Delete test/Bookify.Domain.UnitTests directory

--- test/Bookify.Domain.UnitTests/Users/UserTests.cs
+++ test/Bookify.Domain.UnitTests/Users/UserTests.cs
@@ -1,40 +1,31 @@
 ﻿using Bookify.Domain.Users;
 using Bookify.Domain.Users.Events;
 using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Bookify.Domain.UnitTests.Users
+namespace Bookify.Domain.UnitTests.Users;
+
+public class UserTests 
 {
-   public class UserTests  
+    [Fact]
+    public void Create_Should_SetPropertyValue()
     {
-        [Fact]
-        public void Create_Should_SetPropertyValue()
-        {
-            // Act
-            var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+        // Act
+        var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
 
-            // Assert
-            user.FirstName.Should().Be(UserData.FirstName);
-            user.LastName.Should().Be(UserData.LastName);
-            user.Email.Should().Be(UserData.Email);
-        }
-
-        [Fact]
-        public void Create_Should_RaiseUserCreatedDomainEvent()
-        {
-            // Act
-            var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
-
-            // Assert
-
-          var domainEvent =  user.GetDomainEvents().OfType<UserCreatedDomainEvents>().SingleOrDefault();
-
-            domainEvent.UserId.Should().Be(user.Id);    
-          
-        }
+        // Assert
+        user.FirstName.Should().Be(UserData.FirstName);
+        user.LastName.Should().Be(UserData.LastName);
+        user.Email.Should().Be(UserData.Email);
     }
+
+    [Fact]
+    public void Create_Should_RaiseUserCreatedDomainEvent()
+    {
+        // Act
+        var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+       
+    }
+
+   
 }
